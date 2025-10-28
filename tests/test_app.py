031a92a463e5c0dc660d51e1aff8deb32ac9426d
Leo Aigner <leoaigner7@gmail.com>
2025-10-28 14:41:53 +0100

    Fix: make app importable for pytest

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,5 +1,11 @@
+import os
+import sys
+import importlib
+
+# Damit "app" importierbar ist:
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
 def test_import_app():
     """Check if Flask app can be imported."""
-    import importlib
     app_module = importlib.import_module("app.main")
     assert app_module is not None
